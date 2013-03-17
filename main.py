6525f55e52c8e61093e95afe3e1afad88836f99d
Vahit <vahid.maani@gmail.com>
2013-03-17 10:49:52 +0330

    Startup.

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+###############################################
+# Compare some folders and report same files. #
+# Vahid.Maani [at] gmial [dot] com            #
+###############################################
+
